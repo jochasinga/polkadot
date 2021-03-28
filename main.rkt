c7a1e78eea9b20954e0ff6c15e077b8ed6761340
Joe Chasinga <jo.chasinga@gmail.com>
2021-03-28 04:39:31 -0700

    Add shift to shift color list

--- main.rkt
+++ main.rkt
@@ -2,6 +2,9 @@
 
 #lang typed/racket
 
+(define-type XPlace (U String Symbol))
+(define-type YPlace (U String Symbol))
+
 (require "./color.rkt")
 
 (require/typed 2htdp/image
@@ -11,6 +14,9 @@
                [above (-> Any * Any)]
                [overlay/align (-> XPlace YPlace Any * Any)]
                [make-color (-> Number Number Number Any)]
+;              [x-place? (-> XPlace Boolean)]
+;              [y-place? (-> YPlace Boolean)]
+;              [image? (-> Any Boolean)]
                [place-image (-> Any Real Real Any Any)])
                        
 (require/typed rackunit
@@ -26,6 +32,16 @@
           (make-color (car c)
                       (cadr c)
                       (caddr c))))
+
+;; test "dot"
+(check-equal? (dot 10) (circle 10 MODE "black"))
+(check-equal? (dot 20)
+              (circle 20 MODE "black"))
+(check-equal? (dot 20 (list 234 45 120))
+              (circle 20 MODE (make-color 234 45 120)))
+(check-equal? (dot 20 (hex->rgb "ffffff"))
+              (circle 20 MODE (make-color 255 255 255)))
+
 
 (: dots-aux (-> Real Integer Natural (Listof RGBList) (Listof RGBList) Any))
 (define (dots-aux radius n margin colors acc)
@@ -56,29 +72,29 @@
   (dots-aux radius n margin colors colors))
 
 
+(: shift (All (T) (->* ((Listof T) Integer) ((Listof T)) (Listof T))))
+(define (shift ls n [acc '()])
+  (append
+   (take-right ls (- (length ls) n))
+   (take ls n)))
+
+(check-equal? (shift '(1 2 3 4 5) 2) '(3 4 5 1 2))
+  
 ;; Create a matrix of alternating rows of dots
-(: mdots (-> Real Integer Natural Natural Any))
-(define (mdots radius nx ny margin)
-  (let ([a (dots radius nx margin)]
-        [b (dots radius (- nx 1) margin)])
+(: mdots (->* (Real Integer Natural Natural) ((Listof RGBList)) Any))
+(define (mdots radius nx ny margin [colors (list DEFAULT_COLOR)])
+  (let ([a (dots radius nx margin colors)]
+        [b (dots radius (- nx 1) margin (shift colors (- (length colors) 1)))])
     (if (= ny 0)
         a
         (above
          (if (= 0 (modulo ny 2))
              a
              b)
-         (mdots radius nx (- ny 1) margin)))))
+         (mdots radius nx (- ny 1) margin colors)))))
            
              
-;;;;;;;;;;;;;;;;;; test "dot" ;;;;;;;;;;;;;;;;;;;;;
 
-(check-equal? (dot 10) (circle 10 MODE "black"))
-(check-equal? (dot 20)
-              (circle 20 MODE "black"))
-(check-equal? (dot 20 (list 234 45 120))
-              (circle 20 MODE (make-color 234 45 120)))
-(check-equal? (dot 20 (hex->rgb "ffffff"))
-              (circle 20 MODE (make-color 255 255 255)))
   
       
       
